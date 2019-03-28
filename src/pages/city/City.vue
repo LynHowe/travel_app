d6db6f645af22189261ad285136bb868483ffa21
ZhengLinHao <924819852@qq.com>
2019-03-28 17:22:36 +0800

    search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/Header'
+import CitySearch from './component/Search'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 
---
+++ src/pages/city/component/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或名称">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~@/assets/styles/variable.styl'
+  .search
+    margin-top: -.02rem
+    padding: .1rem .2rem
+    background: $bgColor
+    height: .7rem
+    .search-input
+      text-align: center
+      height: .67rem
+      margin: auto
+      width: 100%
+      border-radius: .1rem
+      box-sizing: border-box
+      padding: .2rem
+      color: #666
+</style>
